2b9dfdf60354aacd222e1100f61ec3952ad2700d
Miles <Milesvbowles@gmail.com>
2019-01-11 16:28:51 -0800

    Added pulse and spinner animations. Added comments.

--- main.ino
+++ main.ino
@@ -1,84 +1,73 @@
-// ---------------------------------------------------------------
+// Author: Miles Bowles
+// Project: Light Touch
+// Description: Light animations triggered by capacitive touch sensor
+
 // Imports
 // ---------------------------------------------------------------
 
 #include <Adafruit_NeoPixel.h>
 
-// ---------------------------------------------------------------
 // References
 // ---------------------------------------------------------------
 
 #define buttonPin 8 // Capacitive touch sensor
 #define lightPin 9 // Addressable RGB array
 
-// Define RGB pins
-int redPin = 6;
-int greenPin = 5;
-int bluePin = 3;
-
-// ---------------------------------------------------------------
 // Variables
 // ---------------------------------------------------------------
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(40, lightPin);
+ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(40, lightPin);
 
-int brightness = 40; // 1/3 brightness
+// Set rgb brightness
+int brightness = 60; // ~1/3 brightness
 
-// ---------------------------------------------------------------
 // Setup
 // ---------------------------------------------------------------
 
 void setup()
 {
+    // Setup addressable RGB array
     pixels.begin();
-    pixels.setBrightness(0); // Set lights to off state 
-    pixels.show();
+    pixels.setBrightness(0); // Set initial state as off
+    pixels.show(); // Update state
+
     // Set button input pin
     pinMode(buttonPin, INPUT);
     digitalWrite(buttonPin, HIGH);
-    // Set LED output pins
-    pinMode(redPin, OUTPUT);
-    pinMode(greenPin, OUTPUT);
-    pinMode(bluePin, OUTPUT); 
-    // Set RGB to off state 
-    analogWrite(redPin, 255);
-    analogWrite(greenPin, 255);
-    analogWrite(bluePin, 255);
 
+    // Begin serial data transmission
     Serial.begin(9600);
 }
 
-// ---------------------------------------------------------------
 // Main loop
 // ---------------------------------------------------------------
 
 void loop()
 {
-
     // Get button event and act accordingly
-    int b = checkButton();
-    if (b == 1)
-        clickEvent();
-    if (b == 2)
-        doubleClickEvent();
-    if (b == 3)
+    int button = checkButton();
+    if (button == 1)
+        pressEvent();
+    if (button == 2)
+        doublePressEvent();
+    if (button == 3)
         holdEvent();
-    if (b == 4)
+    if (button == 4)
         longHoldEvent();
 }
 
-// ---------------------------------------------------------------
 // Events to trigger
 // ---------------------------------------------------------------
 
-void clickEvent()
+void pressEvent()
 {
-    showPositive();
-    Serial.println("Click");
+    Serial.println("Single Tap");
+    pulseAnimation();
 }
-void doubleClickEvent()
+void doublePressEvent()
 {
-    Serial.println("Double Click");
+    Serial.println("Double Tap");
+    spinnerAnimation();
 }
 void holdEvent()
 {
@@ -86,90 +75,95 @@ void holdEvent()
 }
 void longHoldEvent()
 {
-    cleanMode();
     Serial.println("Long Hold");
 }
 
-// ---------------------------------------------------------------
 // Multi functional button
 // ---------------------------------------------------------------
+// Note - debouncing is not neccesary with capacitive touch sensor. 
+// However, I kept this functionality intact so that it can work with a push button if needed.
 
 // Button timing variables
-int debounce = 20;       // ms debounce period to prevent flickering when pressing or releasing the button
-int DCgap = 250;         // max ms between clicks for a double click event
-int holdTime = 1000;     // ms hold period: how long to wait for press+hold event
-int longHoldTime = 3000; // ms long hold period: how long to wait for press+hold event
+int debounce = 20;          // (ms) Debounce period to prevent flickering when pressing or releasing the button 
+int doublePressGap = 250;   // (ms) Max time between press actions for a double press event
+int holdTime = 1000;        // (ms) How long to wait for press & hold event
+int longHoldTime = 3000;    // (ms) How long to wait for long press & hold event
 
 // Button variables
-boolean buttonVal = HIGH;          // value read from button
-boolean buttonLast = HIGH;         // buffered value of the button's previous state
-boolean DCwaiting = false;         // whether we're waiting for a double click (down)
-boolean DConUp = false;            // whether to register a double click on next release, or whether to wait and click
-boolean singleOK = true;           // whether it's OK to do a single click
-long downTime = -1;                // time the button was pressed down
-long upTime = -1;                  // time the button was released
-boolean ignoreUp = false;          // whether to ignore the button release because the click+hold was triggered
-boolean waitForUp = false;         // when held, whether to wait for the up event
-boolean holdEventPast = false;     // whether or not the hold event happened already
-boolean longHoldEventPast = false; // whether or not the long hold event happened already
+boolean buttonState = HIGH;         // Value read from button
+boolean lastButtonState = HIGH;     // Buffered value of the button's previous state
+boolean doublePressWaiting = false; // Waiting for a double press (down)
+boolean doublePressOnUp = false;    // Whether to register a double press on next release
+boolean singlePress = true;         // Whether to register a single click
+long downTime = -1;                 // Time the button was pressed down
+long upTime = -1;                   // Time the button was released
+boolean ignoreUp = false;           // Whether to ignore the button release because the click & hold was triggered
+boolean waitForUp = false;          // When held, whether to wait for the up event
+boolean holdEventPast = false;      // Whether or not the hold event happened already
+boolean longHoldEventPast = false;  // Whether or not the long hold event happened already
 
 int checkButton()
 {
     int event = 0;
-    buttonVal = digitalRead(buttonPin);
+    // Get button state
+    buttonState = digitalRead(buttonPin);
+
     // Button pressed down
-    if (buttonVal == 1 && buttonLast == 0 && (millis() - upTime) > debounce)
+    if (buttonState == 1 && lastButtonState == 0 && (millis() - upTime) > debounce)
     {
         downTime = millis();
         ignoreUp = false;
         waitForUp = false;
-        singleOK = true;
+        singlePress = true;
         holdEventPast = false;
         longHoldEventPast = false;
-        if ((millis() - upTime) < DCgap && DConUp == false && DCwaiting == true)
-            DConUp = true;
+        if ((millis() - upTime) < doublePressGap && doublePressOnUp == false && doublePressWaiting == true)
+            doublePressOnUp = true;
         else
-            DConUp = false;
-        DCwaiting = false;
+            doublePressOnUp = false;
+            doublePressWaiting = false;
     }
     // Button released
-    else if (buttonVal == 0 && buttonLast == 1 && (millis() - downTime) > debounce)
+    else if (buttonState == 0 && lastButtonState == 1 && (millis() - downTime) > debounce)
     {
         if (not ignoreUp)
         {
             upTime = millis();
-            if (DConUp == false)
-                DCwaiting = true;
+            if (doublePressOnUp == false)
+                doublePressWaiting = true;
             else
             {
                 event = 2;
-                DConUp = false;
-                DCwaiting = false;
-                singleOK = false;
+                doublePressOnUp = false;
+                doublePressWaiting = false;
+                singlePress = false;
             }
         }
     }
-    // Test for normal click event: DCgap expired
-    if (buttonVal == 0 && (millis() - upTime) >= DCgap && DCwaiting == true && DConUp == false && singleOK == true && event != 2)
+
+    // Check for normal click event: doublePressGap expired
+    if (buttonState == 0 && (millis() - upTime) >= doublePressGap && doublePressWaiting == true && doublePressOnUp == false && singlePress == true && event != 2)
     {
         event = 1;
-        DCwaiting = false;
+        doublePressWaiting = false;
     }
-    // Test for hold
-    if (buttonVal == 1 && (millis() - downTime) >= holdTime)
+
+    // Check for hold
+    if (buttonState == 1 && (millis() - downTime) >= holdTime)
     {
-        // Trigger "normal" hold
+        // Trigger normal hold
         if (not holdEventPast)
         {
             event = 3;
             waitForUp = true;
             ignoreUp = true;
-            DConUp = false;
-            DCwaiting = false;
-            //downTime = millis();
+            doublePressOnUp = false;
+            doublePressWaiting = false;
+            downTime = millis();
             holdEventPast = true;
         }
-        // Trigger "long" hold
+
+        // Trigger long hold
         if ((millis() - downTime) >= longHoldTime)
         {
             if (not longHoldEventPast)
@@ -179,104 +173,149 @@ int checkButton()
             }
         }
     }
-    buttonLast = buttonVal;
+
+    // Update button state
+    lastButtonState = buttonState;
     return event;
 }
 
-
-// ---------------------------------------------------------------
-// Positve light animation
+// Light Functions
 // ---------------------------------------------------------------
 
-void showPositive()
+// Set RGB colors
+void setLEDColor(int r, int g, int b)
 {
-    digitalWrite(buttonPin, LOW);
-    uint16_t r, j;
+    // Set brightness
     pixels.setBrightness(brightness);
-
-    for (j = 0; j < 256 * 1; j++)
-    {
-        for (r = 0; r < pixels.numPixels(); r++)
-        {
-            pixels.setPixelColor(r, positiveWheel(((r * 256 / pixels.numPixels()) + j) & 255));
-        }
-        pixels.show();
-        delay(5);
-    }
-    pixels.setBrightness(0);
+    // Id#, r value, g value, b value - Only using 3 of the 7 available rgbs
+    pixels.setPixelColor(2, r, g, b);
+    pixels.setPixelColor(4, r, g, b);
+    pixels.setPixelColor(6, r, g, b);
     pixels.show();
-    digitalWrite(buttonPin, HIGH);
 }
 
-uint32_t positiveWheel(byte WheelPos)
-{
-    if (WheelPos < 25)
-    {
-        return pixels.Color(0, 200, 0);
-    }
-    else if (WheelPos < 175)
-    {
-        WheelPos -= 25;
-        return pixels.Color(0, WheelPos * 3, 0);
-    }
-    else
-    {
-        WheelPos -= 25;
-        return pixels.Color(255, 255, 255);
-    }
-}
-
-// ---------------------------------------------------------------
-// Clean mode 
+// Light Animations
 // ---------------------------------------------------------------
 
-void cleanMode()
-{
+// Spinner animation
+void spinnerAnimation() {
+
+    uint16_t i, n;
+
+    // Disable button
     digitalWrite(buttonPin, LOW);
-    uint16_t r, j;
+
+    // Set rgb brightness
     pixels.setBrightness(brightness);
+
+    // Set animation rotation speed (ms)
+    int rotationSpeed = 150;
+
+    // Set color 3 
+    int r1 = 255;
+    int g1 = 255; 
+    int b1 = 255;
+
+    // Set color 3 
+    int r2 = 100;
+    int g2 = 200; 
+    int b2 = 255;
+
+    // Set color 3 
+    int r3 = 0;
+    int g3 = 50; 
+    int b3 = 255;
+
+    for (i = 0; i < 30; i++) 
+    {
+        // Iterate through lights
+        for (n = 0; n < 3; n++)
+        {
+            // Set rgb values for current iteration
+            switch (n)
+            {
+                case 0:
+                    pixels.setPixelColor(2, r1, g1, b1);
+                    pixels.setPixelColor(4, r2, g2, b2);
+                    pixels.setPixelColor(6, r3, g3, b3);
+                    break;
+                case 1:
+                    pixels.setPixelColor(6, r1, g1, b1);
+                    pixels.setPixelColor(2, r2, g2, b2);
+                    pixels.setPixelColor(4, r3, g3, b3);
+                    break;
+                case 2:
+                    pixels.setPixelColor(4, r1, g1, b1);
+                    pixels.setPixelColor(6, r2, g2, b2);
+                    pixels.setPixelColor(2, r3, g3, b3);
+                    break;
+                default:
+                    break;
+            }
+
+            // Update rgb array
+            pixels.show();
+            // Set animation rotation speed
+            delay(rotationSpeed);
+        }
+    }
     
-    // Set lights to purple-blue
-    analogWrite(redPin, 120);
-    analogWrite(greenPin, 255);
-    analogWrite(bluePin, 0);
-
-    // 9 animation cycles
-    for (j = 0; j < 256 * 9; j++)
-    { 
-        for (r = 0; r < pixels.numPixels(); r++)
-        {
-            pixels.setPixelColor(r, cleanModeWheel(((r * 256 / pixels.numPixels()) + j) & 255));
-        }
-        pixels.show();
-        delay(5);
-    }
-    // Set RGB to off state 
-    analogWrite(redPin, 255);
-    analogWrite(greenPin, 255);
-    analogWrite(bluePin, 255);
-    // Turn lights off
+    // Set rgb array to off when animation is finished
     pixels.setBrightness(0);
     pixels.show();
+
+    // Enable button
     digitalWrite(buttonPin, HIGH);
-
 }
 
-uint32_t cleanModeWheel(byte WheelPos)
+// Pulse animation
+void pulseAnimation()
 {
-    if (WheelPos < 40)
-    {
-        return pixels.Color(255, 255, 255);
-    }
-    else if (WheelPos < 180)
-    {
-        WheelPos -= 90;
-        return pixels.Color(0, 0, WheelPos * 3);
-    }
-    else
-    {
-        WheelPos -= 65;
-        return pixels.Color(0, 0, 255 - WheelPos * 3);
-    }
-}
+    uint16_t i, n;
 
+    // Disable button
+    digitalWrite(buttonPin, LOW);
+
+    // Set rgb brightness
+    pixels.setBrightness(brightness);
+
+    // Set pulse speed (ms)
+    int pulseSpeed = 5;
+
+    // TODO: Add logic for pulsing with any input color
+    // Set color 
+    // int r = 255;
+    // int g = 255; 
+    // int b = 255;
+
+    for (i = 0; i < 10; i++) 
+    {
+        // Cycle from 0 brightness to full brightness and back
+        for (n = 0; n < (255 * 2); n++)
+        {
+            // Adjust brightness
+            if (n < 255) {
+                pixels.setPixelColor(4, n, n, n);
+                pixels.setPixelColor(6, n, n, n);
+                pixels.setPixelColor(2, n, n, n);
+            } else {
+                int c = ((255 * 2) - n);
+                pixels.setPixelColor(4, c, c, c);
+                pixels.setPixelColor(6, c, c, c);
+                pixels.setPixelColor(2, c, c, c);
+            }
+
+            // Update rgb array
+            pixels.show();
+            // Set pulse speed
+            delay(pulseSpeed);
+        }
+    }
+    
+    // Set rgb array to off when animation is finished
+    pixels.setBrightness(0);
+    pixels.show();
+
+    // Enable button
+    digitalWrite(buttonPin, HIGH);
+}
